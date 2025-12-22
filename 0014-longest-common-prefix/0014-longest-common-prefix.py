d9772a588442f59a97f001913f85da13c705af0b
BELHADJ AIMEN WAIL <belhadjaymenwael@gmail.com>
2025-12-22 18:37:20 +0100

    Time: 4 ms (18.15%), Space: 12.4 MB (92.37%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        for i, char in enumerate(strs[0]):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != char:
+                    return strs[0][:i]
+        return strs[0]
