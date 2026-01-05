10c79d3a81d545d986d136e591d3e325ae7cdb90
BELHADJ AIMEN WAIL <belhadjaymenwael@gmail.com>
2026-01-06 00:35:14 +0100

    Time: 0 ms (100%), Space: 12.6 MB (0.09%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+#for the tle prblm 
+        if dividend == -2**31 and divisor == -1:
+            return 2**31 - 1
+
+
+
+        if dividend == 0:
+            return 0
+
+       
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
+
+        a = abs(dividend)
+        b = abs(divisor)
+
+        quotient = 0
+#due the time limit exceeded error 
+       # while a >= b:
+        #    a -= b
+         #   quotient += 1
+
+        #return quotient * sign
+        while a >= b:
+            temp = b
+            multiple = 1
+
+            # Double until too large
+            while a >= (temp << 1):
+                temp <<= 1
+                multiple <<= 1
+
+            a -= temp
+            quotient += multiple
+
+        return quotient * sign
